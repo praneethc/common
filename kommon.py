062b7bf87a46ccb1c6dd39e248ae76ac3ae32945
Kaan Akşit <kaksit@nvidia.com>
2017-08-22 17:28:04 -0700

    kommon.py is initiated.

---
+++ kommon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+__author__  = ('Kaan Akşit')
+__version__ = '0.1'
+
+# Definition to prompt.
+def prompt(txt,title='COMMON'):
+    print '[%s] [%s] %s' % (str(datetime.now()),title,txt)
+
+
+# Elem terefiş, kem gözlere şiş!
+if __name__ == '__main__':
+    sys.exit(main())
+
