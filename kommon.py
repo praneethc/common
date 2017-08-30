d3cb3eb638203859ec4ce5d423659e2b4812257d
Kaan Akşit <kaksit@nvidia.com>
2017-08-30 16:33:16 -0700

    Definition to generate a plane perpendicular to Z axis.

--- kommon.py
+++ kommon.py
@@ -11,6 +11,14 @@ __version__ = '0.1'
 def prompt(txt,title='COMMON'):
     print('[%s] [%s] %s' % (str(datetime.now()),title,txt))
 
+# Definition to generate a plane perpendicular to Z axis.
+def generateplane(point):
+    plane = np.array([
+                      [point[0]+10 ,point[1]    ,point[2]],
+                      [point[0]    ,point[1]+10 ,point[2]],
+                      [point[0]    ,point[1]    ,point[2]]
+                     ])
+    return plane
 
 # Elem terefiş, kem gözlere şiş!
 if __name__ == '__main__':
